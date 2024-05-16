09c4583a8f863acb9d3650e67a411796ec988881
cherites0610 <cherites0610@gmail.com>
2024-05-16 14:21:04 +0800

    feat(nav-bar):新增導航欄路由功能

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductCardListComponent } from './product-card-list/product-card-list.component';
+import { ShoppingCarComponent } from './shopping-car/shopping-car.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'productPage', component: ProductCardListComponent},
+    {path: 'shoppingCard', component: ShoppingCarComponent},
+];
---
+++ src/app/shopping-car/shopping-car.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-car',
+  standalone: true,
+  imports: [],
+  templateUrl: './shopping-car.component.html',
+  styleUrl: './shopping-car.component.css'
+})
+export class ShoppingCarComponent {
+
+}
