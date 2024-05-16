e76d67d10eaae8faa5184dd330404a280e81609a
cherites0610 <cherites0610@gmail.com>
2024-05-16 14:14:34 +0800

    feat(nav-bar):新增導航欄界面

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProductCardListComponent],
+  imports: [RouterOutlet,ProductCardListComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
